d42f0e2b29c6210334f3546f2512f0119350b86d
Ruben98Z <43741046+Ruben98Z@users.noreply.github.com>
2020-02-20 18:48:20 +0100

    added new level, gifs and images

--- src/js/game.js
+++ src/js/game.js
@@ -439,6 +439,45 @@ var levels = {
 				{type:"hero", name:"orange",x:80,y:405},
 				{type:"hero", name:"apple",x:140,y:405},
 			]
+		},
+		{   // Tercer nivel
+			foreground:'desert-foreground',
+			background:'clouds-background',
+			entities:[
+				{type:"ground", name:"dirt", x:500,y:440,width:1000,height:20,isStatic:true},
+				{type:"ground", name:"wood", x:185,y:390,width:30,height:80,isStatic:true},
+	
+				{type:"block", name:"wood", x:720,y:380,angle:90,width:100,height:25},
+				{type:"block", name:"wood", x:620,y:380,angle:90,width:100,height:25},
+				{type:"block", name:"glass", x:670,y:317.5,width:122.7,height:25},
+				//Pilar 1
+				{type:"block", name:"wood", x:720,y:254,angle:90,width:100,height:25},
+				{type:"block", name:"wood", x:720,y:155,angle:90,width:100,height:25},
+				{type:"block", name:"glass", x:770,y:92, width:122.7,height:25},
+
+
+				//Pilar 2
+				{type:"block", name:"wood", x:820,y:380,angle:90,width:100,height:25},
+				{type:"block", name:"wood", x:820,y:280,angle:90,width:100,height:25},
+				{type:"block", name:"glass", x:870,y:217.5, width:122.7,height:25,isStatic:true},
+				{type:"block", name:"wood", x:820,y:154,angle:90,width:100,height:25, isStatic:true},
+
+				//Pilar 3
+				{type:"block", name:"wood", x:920,y:380,angle:90,width:100,height:25},
+
+
+
+
+
+				{type:"villain", name:"fries",x:670,y:405,calories:420},
+				{type:"villain", name:"burger",x:920,y:305,calories:420},
+				{type:"villain", name:"sodacan",x:870,y:175,calories:420},
+
+
+				{type:"hero", name:"strawberry",x:30,y:415},
+				{type:"hero", name:"orange",x:80,y:405},
+				{type:"hero", name:"apple",x:140,y:405},
+			]
 		}
 	],
 
